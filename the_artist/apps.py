6346afb57d5fec7aada3ca549759f73638a6af38
AlexandruAprodu <67591188+AlexandruAprodu@users.noreply.github.com>
2020-12-15 16:59:56 +0200

    started 2 apps, added urls

---
+++ the_artist/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TheArtistConfig(AppConfig):
+    name = 'the_artist'
---
+++ the_professional/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TheProfessionalConfig(AppConfig):
+    name = 'the_professional'
