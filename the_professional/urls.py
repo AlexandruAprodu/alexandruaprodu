2ca4ade6160d72c5f7afedbb0afaa1121cd41464
AlexandruAprodu <67591188+AlexandruAprodu@users.noreply.github.com>
2020-12-15 17:41:54 +0200

    added some info in app urls, Procfile, .env

---
+++ the_professional/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from the_artist import views as the_artist_views
+from the_professional import views as the_professional_views
+from django.contrib.auth import views as auth_views
+
+app_name = 'the_professional'
+
+urlpatterns = [
+    path(),
+]
