ae84953f3d37901d205d9d780afdbf9ff2d6cf21
AlexandruAprodu <67591188+AlexandruAprodu@users.noreply.github.com>
2021-01-06 17:44:35 +0200

    frontend template 70% done, without static routes

--- alexandruaprodu/urls.py
+++ alexandruaprodu/urls.py
@@ -4,14 +4,14 @@ from django.contrib import admin
 from django.urls import path, include
 from the_artist import urls as the_artist_urls
 from home import urls as home_urls
-# from the_professional import urls as the_professional_urls
+from the_professional import urls as the_professional_urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('the_artist/', include(the_artist_urls)),
     path('', include(home_urls)),
-    # path('the_professional/', include(the_professional_urls)),
+    path('the_professional/', include(the_professional_urls)),
 
 ]
 if settings.DEBUG is True:
--- the_professional/views.py
+++ the_professional/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'the_professional/index.html')
