edacb3fbbaf35716c408cf4a205f4d318cab9c30
AlexandruAprodu <67591188+AlexandruAprodu@users.noreply.github.com>
2021-01-15 18:27:03 +0200

    colllect static

---
+++ staticfiles/the_artist/js/click.js
@@ -0,0 +1,14 @@
+ document.getElementById('stanga').addEventListener('click', function() {
+
+    document.location.href = url1
+    }, false);
+
+ document.getElementById('mijloc').addEventListener('click', function() {
+
+    document.location.href = url2
+    }, false);
+
+ document.getElementById('dreapta').addEventListener('click', function() {
+
+    document.location.href = url3
+    }, false);
