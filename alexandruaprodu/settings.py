3b63c4c932b8816597ae8da87267ac11f849a818
AlexandruAprodu <67591188+AlexandruAprodu@users.noreply.github.com>
2021-01-15 18:50:53 +0200

    added www.alexandruaprodu.me 1

--- alexandruaprodu/settings.py
+++ alexandruaprodu/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['http://127.0.0.1:8000', 'alexandruaprodu.herokuapp.com', 'alexandruaprodu.me']
+ALLOWED_HOSTS = ['http://127.0.0.1:8000', 'alexandruaprodu.herokuapp.com', 'www.alexandruaprodu.me']
 
 # Application definition
 
