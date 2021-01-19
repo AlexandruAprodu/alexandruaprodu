747e54d457bc2d6d1698cb359b2df5b453f810e8
AlexandruAprodu <67591188+AlexandruAprodu@users.noreply.github.com>
2021-01-19 22:14:13 +0200

    final v2

--- alexandruaprodu/settings.py
+++ alexandruaprodu/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1:8000', 'alexandruaprodu.herokuapp.com', 'www.alexandruaprodu.me']
+ALLOWED_HOSTS = ['127.0.0.1', 'alexandruaprodu.herokuapp.com', 'www.alexandruaprodu.me']
 
 # Application definition
 
